42510d92d04c9ab8c6022c0da6393daa1061eec5
José Nelson Cultri <josefcultrifrc@gmail.com>
2022-06-13 20:40:18 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/josenelson/workshopmongo/config/Instantiation.java
+++ src/main/java/com/josenelson/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.josenelson.workshopmongo.config;
 import com.josenelson.workshopmongo.domain.Post;
 import com.josenelson.workshopmongo.domain.User;
 import com.josenelson.workshopmongo.dto.AuthorDTO;
+import com.josenelson.workshopmongo.dto.CommentDTO;
 import com.josenelson.workshopmongo.repository.PostRepository;
 import com.josenelson.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +35,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, Instant.parse("2018-03-21T00:00:00Z"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, Instant.parse("2018-03-23T00:00:00Z"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem, mano!", Instant.parse("2018-03-21T00:00:00Z"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", Instant.parse("2018-03-22T00:00:00Z"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", Instant.parse("2018-03-23T00:00:00Z"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().add(c3);
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/com/josenelson/workshopmongo/domain/Post.java
+++ src/main/java/com/josenelson/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.josenelson.workshopmongo.domain;
 
 import com.josenelson.workshopmongo.dto.AuthorDTO;
+import com.josenelson.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -16,6 +19,7 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -66,6 +70,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
