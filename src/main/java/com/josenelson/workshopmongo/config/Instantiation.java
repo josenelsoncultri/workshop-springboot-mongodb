dbb8575868454db66676808e88c937bf3524c1aa
José Nelson Cultri <josefcultrifrc@gmail.com>
2022-06-13 19:22:51 -0300

    Projeção dos dados do autor com DTO (os objetos de usuário precisam ser commitados primeiro para que o ID do usuário exista no MongoDB, ao associá-los com o Post

--- src/main/java/com/josenelson/workshopmongo/config/Instantiation.java
+++ src/main/java/com/josenelson/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.josenelson.workshopmongo.config;
 
 import com.josenelson.workshopmongo.domain.Post;
 import com.josenelson.workshopmongo.domain.User;
+import com.josenelson.workshopmongo.dto.AuthorDTO;
 import com.josenelson.workshopmongo.repository.PostRepository;
 import com.josenelson.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,10 +30,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, Instant.parse("2018-03-21T00:00:00Z"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, Instant.parse("2018-03-23T00:00:00Z"), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, Instant.parse("2018-03-21T00:00:00Z"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, Instant.parse("2018-03-23T00:00:00Z"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
