d2841a7fc593e6d2428ec70f3a9c12b2e310b5af
José Nelson Cultri <josefcultrifrc@gmail.com>
2022-06-15 23:55:04 -0300

    Consulta simples com query methods

--- src/main/java/com/josenelson/workshopmongo/resources/PostResource.java
+++ src/main/java/com/josenelson/workshopmongo/resources/PostResource.java
@@ -1,10 +1,14 @@
 package com.josenelson.workshopmongo.resources;
 
 import com.josenelson.workshopmongo.domain.Post;
+import com.josenelson.workshopmongo.resources.util.URL;
 import com.josenelson.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URLDecoder;
+import java.util.List;
 
 @RequestMapping(value="/posts")
 @RestController
@@ -17,5 +21,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping(value="/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
